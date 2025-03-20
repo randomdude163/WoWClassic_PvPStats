d7efe1685c011470d6fbbb1dd890079014e5d557
randomdude <randomdude163@posteo.org>
2025-03-20 22:47:21 +0100

    Fixed slash commands after refactor

--- PvPStatsClassic/PvPStatsClassic.lua
+++ PvPStatsClassic/PvPStatsClassic.lua
@@ -2,6 +2,7 @@ local function PSC_RegisterSlashCommands()
     SLASH_PVPSTATSCLASSIC1 = "/pvpstatsclassic"
     SLASH_PVPSTATSCLASSIC2 = "/psc"
     SlashCmdList["PVPSTATSCLASSIC"] = PSC_SlashCommandHandler
+    print("Slashcommands registered")
 end
 
 local function Main()
