607212d2e1221c5b84e4b1a1539aea21364f18ac
randomdude <randomdude163@posteo.org>
2025-03-25 00:34:59 +0100

    Moved screenshots into subfolder

--- zip_addon.sh
+++ zip_addon.sh
@@ -1,7 +1,9 @@
+rm PvPStatsClassic.zip
 mkdir _zip
 cp -r PvPStatsClassic _zip
 cd _zip
-rm -r PvPStatsClassic/img
+rm -r PvPStatsClassic/img/Screenshots
+find . -name "*.DS_Store" -type f -delete
 zip -r PvPStatsClassic.zip PvPStatsClassic
 mv PvPStatsClassic.zip ..
 cd ..
