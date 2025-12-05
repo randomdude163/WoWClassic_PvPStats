b4025b18a15eec50cda2e89cc1fd381476a2c007
randomdude <randomdude163@posteo.org>
2025-12-05 13:41:33 +0100

    version bump

--- PvPStatsClassic/ConfigUI.lua
+++ PvPStatsClassic/ConfigUI.lua
@@ -984,7 +984,7 @@ local function CreateAboutTab(parent)
 
     local versionText = parent:CreateFontString(nil, "OVERLAY", "GameFontNormal")
     versionText:SetPoint("TOP", header, "BOTTOM", 0, -5)
-    versionText:SetText("Version: 2.4")
+    versionText:SetText("Version: 2.4.1")
     versionText:SetTextColor(1, 1, 1)
 
     local creditsHeader = parent:CreateFontString(nil, "OVERLAY", "GameFontNormalLarge")
